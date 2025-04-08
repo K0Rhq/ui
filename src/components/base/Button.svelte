a5b632dbc983017adb6cbc29cb2af6418a6f858d
dukc <dukkcc@proton.me>
2025-04-08 13:18:06 -0700

    asd

--- src/components/base/Button.svelte
+++ src/components/base/Button.svelte
@@ -1,3 +1,32 @@
-<button class="fg-1 rounded-xl px-8 py-2">
-  <slot />
+<script lang="ts">
+    import type { HTMLButtonAttributes } from "svelte/elements";
+    import { cva, type VariantProps } from "class-variance-authority";
+
+    const button = cva(
+        "border border-stroke-2 rounded-full px-16 py-8 hover:scale-105 duration-150 ease-out active:scale-95",
+        {
+            variants: {
+                variant: {
+                    primary: "bg-accent text-text-reversed-solid",
+                    secondary: "bg-fg-2 text-text-solid",
+                    success: "bg-green text-text-reversed-solid",
+                    warning: "bg-yellow text-text-reversed-solid",
+                    danger: "bg-red text-text-reversed-solid",
+                },
+            },
+        },
+    );
+
+    interface $$Props
+        extends HTMLButtonAttributes,
+            VariantProps<typeof button> {}
+
+    // default variants
+    export const variant: $$Props["variant"] = "primary";
+
+    console.log("PROPS", variant);
+</script>
+
+<button {...$$props} class={button({ variant })}>
+    <slot />
 </button>
