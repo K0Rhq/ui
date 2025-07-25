93ae0d57eb0e025e9eee17841d2ace78a23fd60d
dukc <dukkcc@proton.me>
2025-07-25 13:27:09 -0700

    start button (still need to style)

---
+++ src/stories/button.stories.svelte
@@ -0,0 +1,32 @@
+<script module>
+    import { defineMeta } from "@storybook/addon-svelte-csf";
+    import { Button } from "$lib/components/ui/button/index.ts";
+
+    const { Story } = defineMeta({
+        title: "Components/Button",
+        tags: ["autodocs"],
+        component: Button,
+        argTypes: {
+            label: {
+                control: { type: "text" },
+            },
+        },
+        args: {
+            label: "Button",
+        },
+        parameters: {
+            controls: {
+                include: Object.keys({
+                    label: null,
+                }),
+                hideNoControlsWarning: true,
+            },
+        },
+    });
+</script>
+
+{#snippet template(args)}
+    <Button {...args}>{args.label}</Button>
+{/snippet}
+
+<Story name="Button" let:args {template} />
