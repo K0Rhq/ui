ebdbf191c47794a7b169437098c579e576602f96
dukc <dukkcc@proton.me>
2025-07-31 13:05:48 -0700

    add svelte-package builder and packager script

---
+++ src/lib/index.ts
@@ -0,0 +1,14 @@
+// UI Components
+export * as Button from "./components/ui/button/index.js";
+export * as Checkbox from "./components/ui/checkbox/index.js";
+export * as Input from "./components/ui/input/index.js";
+export * as Label from "./components/ui/label/index.js";
+export * as ListItem from "./components/ui/list-item/index.js";
+export * as RadioGroup from "./components/ui/radio-group/index.js";
+export * as Select from "./components/ui/select/index.js";
+export * as Separator from "./components/ui/separator/index.js";
+export * as Slider from "./components/ui/slider/index.js";
+export * as Switch from "./components/ui/switch/index.js";
+
+// Utilities
+export * from "./utils.js";
